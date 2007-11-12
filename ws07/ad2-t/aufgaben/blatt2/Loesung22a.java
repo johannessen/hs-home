59e0739d0ef471f161810055f6506d1cc9a3e47e
Arne Johannessen <joar0011@hs-karlsruhe.de>
2007-11-12 22:09:23 +0000

    aufsplittung in a, b, c fertig

---
+++ ws07/ad2-t/aufgaben/blatt2/Loesung22a.java
@@ -0,0 +1,64 @@
+/* $Id: Loesung22a.java,v 1.1 2007-11-12 22:09:23 arne Exp $
+ * by Arne Johannessen
+ * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
+ */
+
+
+/**
+ * Loesungsvorschlag fuer Aufgabe 2-2a. Scan-Line--Loesung fuer
+ * das Maximum-Sub-Array--Problem mit O(n)-Effizienz.
+ * 
+ * @see <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/ws07/ad2-t/aufgaben/blatt2/">Aufgabenblatt 2</A>
+ * @author <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/">Arne Johannessen</A>
+ * @version $Revision: 1.1 $
+ */
+public class Loesung22a {
+	
+	
+	
+	/**
+	 * Loest das Maximum-Sub-Array--Problem fuer den uebergebenen
+	 * Array im Scan-Line--Verfahren. Bei einem einzigen Durchlauf
+	 * des Arrays wird eine Teilfolge mit der hoechsten Summe
+	 * ermittelt. Der Wert dieser Summe wird auf dem
+	 * Standard-Ausgabe-Stream ausgegeben.
+	 * 
+	 * @param array der fuer die Bestimmung der Problemloesung
+	 * heranzuziehende Gesamt-Array
+	 */
+	public static void findeLoesung (int[] array) {
+		
+		// lokale Variablen initialisieren
+		int sum = 0;
+		int maximumSum = 0;
+		
+		// Gesamt-Array genau einmal durchlaufen
+		for (int index = 0; index < array.length; index++) {
+			
+			// laufende Summe fortfuehren
+			sum += array[index];
+			
+			// negative Summe kann nie maximal werden, daher nullen
+			if (sum < 0) {
+				sum = 0;
+			}
+			
+			// aktuelle laufende Summe merken, falls bisher hoechste
+			if (sum > maximumSum) {
+				maximumSum = sum;
+			}
+		}
+		
+		// Ergebnis ausgeben
+		System.out.println("Maximale Teilsumme: "+maximumSum);  // 59
+	}
+	
+	
+	
+	/** Treiber fuer Aufruf von der Kommandozeilenschnittstelle. */
+	public static void main (String[] args) {
+		int[] meinArray = {-8, 12, -23, 17, 42, -61, 7};
+		findeLoesung(meinArray);
+	}
+	
+}
--- ws07/ad2-t/aufgaben/blatt2/Loesung22c.java
+++ ws07/ad2-t/aufgaben/blatt2/Loesung22c.java
@@ -1,4 +1,4 @@
-/* $Id: Loesung22c.java,v 1.1 2007-11-12 21:34:17 arne Exp $
+/* $Id: Loesung22c.java,v 1.2 2007-11-12 22:09:23 arne Exp $
  * by Arne Johannessen
  * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
  */
@@ -10,7 +10,7 @@
  * 
  * @see <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/ws07/ad2-t/aufgaben/blatt2/">Aufgabenblatt 2</A>
  * @author <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/">Arne Johannessen</A>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class Loesung22c implements MaximumSubArraySolver {
 	
@@ -35,51 +35,44 @@ public class Loesung22c implements MaximumSubArraySolver {
 	public SubArray findMaximumSubArray (int[] array) {
 		
 		// lokale Variablen initialisieren
-		int currentSum = 0;
-		int currentStart = 0;
-		int currentLength = 0;
+		int sum = 0;
 		int maximumSum = 0;
-		int maximumStart = 0;
-		int maximumLength = 0;
+		int beginIndex = 0;  // Startannahme: Teilfolge beginnt bei 0
+		int maximumBegin = -1;
+		int maximumEnd = -1;
 		
 		// Gesamt-Array genau einmal durchlaufen
 		for (int index = 0; index < array.length; index++) {
 			
 			// laufende Summe fortfuehren
-			currentSum += array[index];
-			currentLength++;
+			sum += array[index];
 			
-			// ist aktuelle laufende Summe negativ?
-			if (currentSum < 0) {
-				// laufende Summe auf null setzen
-				currentSum = 0;
-				// mit naechstem Element weitermachen
-				currentStart = index + 1;
-				currentLength = 0;
-				continue;
+			// negative Summe kann nie maximal werden, daher nullen
+			if (sum < 0) {
+				sum = 0;
+				beginIndex = index + 1;
 			}
 			
 			// aktuelle laufende Summe merken, falls bisher hoechste
-			if (currentSum > maximumSum) {
-				maximumSum = currentSum;
-				maximumStart = currentStart;
-				maximumLength = currentLength;
+			if (sum > maximumSum) {
+				maximumSum = sum;
+				maximumBegin = beginIndex;
+				maximumEnd = index;
 			}
 		}
 		
 		// Ergebnis zurueckgeben
-		return new SubArray(array, maximumStart, maximumLength);
+		return new SubArray(array, maximumBegin, maximumEnd - maximumBegin + 1);
 	}
 	
 	
 	
 	/** Treiber fuer Aufruf von der Kommandozeilenschnittstelle. */
 	public static void main (String[] args) {
-		int[] array = null;
-		SubArray maximumSubArray = null;
+		int[] myArray = SubArray.createRandomArray();
+		SubArray maximumSubArray = new Loesung22c().findMaximumSubArray(myArray);
 		
-		array = SubArray.createRandomArray();
-		maximumSubArray = new Loesung22c().findMaximumSubArray(array);
+		// Ergebnis ausgeben
 		System.out.println(maximumSubArray);
 		System.out.println("Summe: ["+maximumSubArray.getSum()+"]");
 	}
