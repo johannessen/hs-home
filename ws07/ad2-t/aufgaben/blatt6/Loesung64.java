8a09816b27d85268a414e5e41e7dd8872d017247
Arne Johannessen <joar0011@hs-karlsruhe.de>
2007-12-12 03:49:10 +0000

    some limited code cleanup

--- ws07/ad2-t/aufgaben/blatt6/Loesung64.java
+++ ws07/ad2-t/aufgaben/blatt6/Loesung64.java
@@ -1,4 +1,4 @@
-/* $Id: Loesung64.java,v 1.1 2007-12-12 02:58:40 arne Exp $
+/* $Id: Loesung64.java,v 1.2 2007-12-12 03:49:10 arne Exp $
  * by Arne Johannessen
  * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
  */
@@ -10,24 +10,34 @@
  * 
  * @see <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/ws07/ad2-t/aufgaben/blatt6/">Aufgabenblatt 6</A>
  * @author <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/">Arne Johannessen</A>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class Loesung64 {
-	
 	
 	private Stack left;
 	private Stack center;
 	private Stack right;
-	private Stack[] towers = null;
 	
+	private Stack[] towers;
+	
+	
+	/** konstruktor */
 	public Loesung64 () {
+		
+		// Pfosten erzeugen
 		left = new Stack("[Links]");
 		center = new Stack("[Mitte]");
 		right = new Stack("[Rechts]");
+		
+		// einen Array fuer indizierten Pfostenzugriff bereitstellen
 		towers = new Stack[] {left, center, right};
 	}
 	
 	
+	
+	
+	
+	/** oberste Scheibe von from nach to bewegen */
 	public void moveDisc (Stack from, Stack to) {
 		String disc = from.pop();
 		to.push(disc);
@@ -36,6 +46,7 @@ public class Loesung64 {
 	}
 	
 	
+	/** Turm der Hoehe height von from nach to bewegen, mit Hilfspfosten temp */
 	public void moveTower (int height, Stack from, Stack to, Stack temp) {
 		if (height <= 0) {
 			return;
@@ -46,6 +57,7 @@ public class Loesung64 {
 	}
 	
 	
+	/** Spiel loesen fuer die uebergebene Liste von Scheiben */
 	public void solvePuzzle (String[] discs) {
 		
 		// Pfosten und Turm vorbereiten
@@ -62,6 +74,10 @@ public class Loesung64 {
 	}
 	
 	
+	
+	
+	
+	/** alle Pfosten mitsamt Scheiben in ASCII-Grafik ausgeben */
 	public void printAllTowers () {
 		final int WIDTH = 20;
 		final char AIR = ' ';
@@ -73,10 +89,10 @@ public class Loesung64 {
 		for (int index = 0; index < iterators.length; index++) {
 			iterators[index] = towers[index].iterator();
 		}
-		for (int height = maxHeight; height >= 0; height--) {
+		for (int height = maxHeight; height > 0; height--) {
 			for (int index = 0; index < towers.length; index++) {
 				String level = "";
-				if (towers[index].size() > height) {
+				if (towers[index].size() >= height) {
 					level = (String)iterators[index].next();
 				}
 				System.out.print(space(level, WIDTH, AIR)+SPACER);
@@ -94,6 +110,8 @@ public class Loesung64 {
 		System.out.println();
 	}
 	
+	
+	/** den STring text bis zur Laenge width mit space auffuellen */
 	private String space (String text, int width, char space) {
 		StringBuffer buffer = new StringBuffer(width);
 		buffer.append(text);
@@ -104,17 +122,41 @@ public class Loesung64 {
 	}
 	
 	
+	
+	
+	
 	/** Treiber fuer Aufruf von der Kommandozeilenschnittstelle. */
 	public static void main (String[] args) {
+		
+		// Scheiben erzeugen
+		String[] discs;
+		if (args.length == 0) {
+			discs = new String[] {"*", "**", "***"};
+		}
+		else {
+			// Scheibenzahl von Kommandozeile einlesen
+			int discCount = Integer.parseInt(args[0]);
+			discs = new String[discCount];
+			for (int discIndex = 0; discIndex < discCount; discIndex++) {
+				StringBuffer buffer = new StringBuffer(discIndex + 1);
+				for (int asterisk = 1; asterisk <= discIndex + 1; asterisk++) {
+					buffer.append('*');
+				}
+				discs[discIndex] = buffer.toString();
+			}
+		}
+		
+		// Spiel loesen
 		Loesung64 hanoi = new Loesung64();
-		String[] discs = new String[] {"*", "**", "***"};
 		hanoi.solvePuzzle(discs);
 	}
 	
 	
 	
-	// um mit einfachen Mitteln eine brauchbare Ausgabe zu bekommen, ist es praktisch, den Stapeln einen Namen geben zu koennen
-	class Stack extends Loesung63 {
+	
+	
+	/** um mit einfachen Mitteln eine brauchbare Ausgabe zu bekommen, ist es praktisch, den Stapeln einen Namen geben zu koennen */
+	protected class Stack extends Loesung63 {
 		
 		private final String name;
 		
