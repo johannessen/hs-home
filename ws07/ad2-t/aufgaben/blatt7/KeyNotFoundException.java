fd7e87c44f2b84a4aed417bb3d446b544beb0e54
Arne Johannessen <joar0011@hs-karlsruhe.de>
2007-12-12 00:37:46 +0000

    initial import

---
+++ ws07/ad2-t/aufgaben/blatt7/KeyNotFoundException.java
@@ -0,0 +1,12 @@
+/* $Id: KeyNotFoundException.java,v 1.1 2007-12-12 00:37:46 arne Exp $
+ * by Arne Johannessen
+ * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
+ */
+
+
+/**
+ * Wird geworfen um anzuzeigen, dass ein Suchalgorithmus das Gesuchte
+ * nicht gefunden hat.
+ */
+public class KeyNotFoundException extends RuntimeException {
+}
