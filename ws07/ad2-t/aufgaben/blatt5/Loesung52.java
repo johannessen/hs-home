aa86cb7077ce419bd032c1ac38e53cc4474e0df8
Arne Johannessen <joar0011@hs-karlsruhe.de>
2008-01-11 02:32:46 +0000

    initial add

---
+++ ws07/ad2-t/aufgaben/blatt5/Loesung52.java
@@ -0,0 +1,49 @@
+/* $Id: Loesung52.java,v 1.1 2008-01-11 02:32:46 arne Exp $
+ * by Arne Johannessen
+ * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
+ */
+
+
+/**
+ * Quellcode fuer Aufgabe 5-2.
+ * 
+ * @see <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/ws07/ad2-t/aufgaben/blatt5/#aufgabe5-2">Aufgabenblatt 5, Aufgabe 5-2</A>
+ * @author <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/">Arne Johannessen</A>
+ * @version $Revision: 1.1 $
+ */
+public class Loesung52 {
+	
+	
+	static {
+		
+		int a;a = 5;
+		
+		int b = 5;
+		
+//		int c,c = 5;
+		
+//		float d = 5,5;
+		
+		float e = 5;
+		
+//		double f1 = 5d; float f2 = f1;
+		
+//		double g1 = 5e; float g2 = g1;
+		
+//		double h1 = 5f; float h2 = h1;
+		
+//		long i = 0.0;
+		
+//		float j = 0.0;
+		
+		float k1 = 5f; double k2 = k1;
+		
+		double l = 0.0;
+		
+		long m = (short)Integer.MAX_VALUE;
+		
+//		short n = (long)0;
+		
+	}
+	
+}
