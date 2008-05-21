f775128b9007c5c3b63aacdf0e3087ce3b6c65be
Arne Johannessen <joar0011@hs-karlsruhe.de>
2008-05-21 01:39:46 +0000

    initial

---
+++ ss08/ad2-t/aufgaben/blatt3/KeyNotFoundException.java
@@ -0,0 +1,12 @@
+/* $Id: KeyNotFoundException.java,v 1.1 2008-05-21 01:39:46 aj3 Exp $
+ * by Arne Johannessen
+ * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
+ */
+
+
+/**
+ * Wird geworfen um anzuzeigen, dass ein Suchalgorithmus das Gesuchte
+ * nicht gefunden hat.
+ */
+public class KeyNotFoundException {
+}
