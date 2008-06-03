5cea8de8309e70bbbf89eabfd2e1071413e1dcf5
Arne Johannessen <joar0011@hs-karlsruhe.de>
2008-06-03 23:56:06 +0000

    +blatt6

---
+++ ss08/ad2-t/aufgaben/blatt6/Loesung63.java
@@ -0,0 +1,36 @@
+/* $Id: Loesung63.java,v 1.1 2008-06-03 23:54:14 aj3 Exp $
+ * by Arne Johannessen
+ * Faculty of Geomatics, Hochschule Karlsruhe - Technik und Wirtschaft
+ */
+
+
+/**
+ * Loesungsvorschlag fuer Aufgabe 6-3. Programm zur naiven Loesung
+ * (brute-force) des Maximum-Sub-Array--Problems mit O(n^3)-Effizienz.
+ * <p>
+ * Dieses Programm ist aus zwei Gruenden voellig untauglich fuer alle
+ * praktischen Zwecke: Erstens ist der Array "hart gecoded", d. h. fest
+ * eincompiliert und nicht zur Laufzeit aenderbar; eigentlich muesste der
+ * Array ueber die Kommandozeilenparamater o. ae. eingelesen werden.
+ * Zweitens ist das Ergebnis keine eindeutige Definition des Sub-Arrays;
+ * eigentlich muesste statt der Summe der Beginn-Index und die Anzahl der
+ * Elemente des Sub-Arrays ausgegeben werden. Auf beides wurde aus
+ * Zeitgruenden verzichtet.
+ * 
+ * @see <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/ss08/ad2-t/aufgaben/blatt6/">Aufgabenblatt 6</A>
+ * @author <A HREF="http://www.home.hs-karlsruhe.de/~joar0011/">Arne Johannessen</A>
+ * @version $Revision: 1.1 $
+ */
+class Loesung63 {
+	
+	
+	
+	/** Treiber fuer Aufruf von der Kommandozeilenschnittstelle. */
+	public static void main (String[] args) {
+		int[] myArray = new int[] {-8, 12, -23, 17, 42, -61, 7};
+		Loesung61 instance = new Loesung61();
+		int maximumSum = instance.findMaximumSubArray(myArray);
+		System.out.println("Maximale Teilsumme: "+maximumSum);  // 59
+	}
+	
+}
